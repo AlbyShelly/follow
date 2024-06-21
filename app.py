9ade353cfe59d5c33745871ea13bf928049be31e
AlbyShelly <albyshelly4@gmail.com>
2024-06-21 20:10:00 +0530

    added kindof compeleted a current status page

--- app.py
+++ app.py
@@ -115,3 +115,19 @@ def new_work():
         #get the evangelist names and ids
         evangelists = db.execute(""" SELECT "id","name" FROM "evangelists" """)
         return render_template("new_work.html", evangelists=evangelists)
+
+@app.route("/current_status")
+def current_status():
+    
+    #query to get all the important details
+    rows =db.execute( """   SELECT "ename", "name" AS "cname", "age" AS "cage", "address", "last_contacted", "cid"
+                            FROM "contacts" JOIN
+	                            (SELECT "evangelists"."id" AS "eid", "evangelists"."name" AS "ename", "contact_id" AS "cid"
+	                            FROM "evangelists" JOIN "follow"
+            	                ON "evangelists"."id" = "follow"."evangelist_id") AS "t1"
+                            ON "contacts"."id" = "t1"."cid" 
+          """
+    )
+    
+    return render_template("current_status.html", rows=rows)
+
---
+++ sql/all_work.sql
@@ -0,0 +1,6 @@
+SELECT "ename", "name" AS "cname", "age" AS "cage", "address", "last_contacted", "cid"
+FROM "contacts" JOIN 
+	(SELECT "evangelists"."id" AS "eid", "evangelists"."name" AS "ename", "contact_id" AS "cid"
+	FROM "evangelists" JOIN "follow"
+	ON "evangelists"."id" = "follow"."evangelist_id") AS "t1"
+ON "contacts"."id" = "t1"."cid"
